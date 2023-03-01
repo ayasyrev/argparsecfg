c040b3cb223e742f52cf085bdf96f7baa3185ad3
ayasyrev <a.yasyrev@gmail.com>
2023-03-01 21:01:30 +0300

    tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -15,13 +15,35 @@ def compare_parsers(
     return True
 
 
+def compare_parsers_actions(
+    parser_1: argparse.ArgumentParser,
+    parser_2: argparse.ArgumentParser,
+) -> bool:
+    """Compare actions at two parsers"""
+    actions_1 = parser_1._actions  # pylint: disable=protected-access
+    actions_2 = parser_2._actions  # pylint: disable=protected-access
+    if len(actions_1) != len(actions_2):
+        return False
+    # check order of elements
+    for act_1, act_2 in zip(actions_1, actions_2):
+        # pass
+        if act_1.__dict__.keys() != act_2.__dict__.keys():  # is it possible?
+            return False
+        if not all(
+            val == act_2.__dict__.get(key)
+            for key, val in act_1.__dict__.items() if key != "container"
+        ):
+            return False
+    return True
+
+
 def test_create_parser_default():
     """test creation basic parser w/ default args"""
     parser_base = argparse.ArgumentParser()
     parser_cfg = ParserCfg()
     parser = create_parser(parser_cfg=parser_cfg)
     assert compare_parsers(parser_base, parser)
-    assert len(parser_base._actions) == len(parser._actions)
+    assert compare_parsers_actions(parser_base, parser)
 
 
 def test_create_parser():
@@ -41,4 +63,4 @@ def test_create_parser():
     )
     parser = create_parser(parser_cfg=parser_cfg)
     assert compare_parsers(parser_base, parser)
-    assert len(parser_base._actions) == len(parser._actions)
+    assert compare_parsers_actions(parser_base, parser)
