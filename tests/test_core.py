dd44f38bf97feca8ccc834b8c1298c2e7206ef31
ayasyrev <a.yasyrev@gmail.com>
2023-03-01 19:06:18 +0300

    tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,13 +6,22 @@ from argparsecfg.core import ParserCfg, create_parser
 def compare_parsers(
     parser_1: argparse.ArgumentParser,
     parser_2: argparse.ArgumentParser,
-    ) -> bool:
+) -> bool:
     """compare two parsers, return True if equal."""
     for attr in parser_1.__dict__:
         if not attr.startswith("_"):
             if getattr(parser_1, attr) != getattr(parser_2, attr):
                 return False
     return True
+
+
+def test_create_parser_default():
+    """test creation basic parser w/ default args"""
+    parser_base = argparse.ArgumentParser()
+    parser_cfg = ParserCfg()
+    parser = create_parser(parser_cfg=parser_cfg)
+    assert compare_parsers(parser_base, parser)
+    assert len(parser_base._actions) == len(parser._actions)
 
 
 def test_create_parser():
@@ -31,7 +40,5 @@ def test_create_parser():
         epilog=epilog,
     )
     parser = create_parser(parser_cfg=parser_cfg)
-    assert parser.prog == parser_base.prog
-    assert parser.description == parser_base.description
-    assert parser.epilog == parser_base.epilog
     assert compare_parsers(parser_base, parser)
+    assert len(parser_base._actions) == len(parser._actions)
