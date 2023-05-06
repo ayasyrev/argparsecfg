00292240dc1be375342dca38219bd492f29f3ad8
ayasyrev <a.yasyrev@gmail.com>
2023-05-06 12:16:51 +0300

    tests refactor - tools  moved to src, diff for actions

--- src/argparsecfg/test_tools.py
+++ src/argparsecfg/test_tools.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import Any, Dict, List, Tuple
 
 
 def parsers_args_equal(
@@ -13,24 +14,66 @@ def parsers_args_equal(
     return True
 
 
+def actions_same(action1: argparse.Action, action2: argparse.Action) -> bool:
+    return type(action1) == type(action2)
+
+
+def actions_equal(action1: argparse.Action, action2: argparse.Action) -> bool:
+    """Compare actions at two parsers"""
+    if not actions_same(action1, action2):
+        return False
+    return all(
+        val == action2.__dict__[key]
+        for key, val in action1.__dict__.items()
+        if key != "container"
+    )
+
+
+def actions_diff(action1: argparse.Action, action2: argparse.Action) -> Dict[str, Tuple[Any, Any]]:
+    """Compare actions at two parsers"""
+    if actions_same(action1, action2):
+        return {
+            key: (val, action2.__dict__[key])
+            for key, val in action1.__dict__.items()
+            if key != "container" and val != action2.__dict__[key]
+        }
+    else:
+        return {}
+
+
 def parsers_actions_equal(
     parser_1: argparse.ArgumentParser,
     parser_2: argparse.ArgumentParser,
 ) -> bool:
+    """Compare actions at two parsers.
+    Parsers equal if same actions and same order."""
+    # if same args but different order will be False - need another check
+    # pylint: disable=protected-access
+    actions_1 = parser_1._actions
+    actions_2 = parser_2._actions
+    if len(actions_1) != len(actions_2):
+        return False
+    for act_1, act_2 in zip(actions_1, actions_2):
+        if not actions_equal(act_1, act_2):
+            return False
+    return True
+
+
+def parsers_actions_diff(
+    parser_1: argparse.ArgumentParser,
+    parser_2: argparse.ArgumentParser,
+) -> List[Dict[str, Tuple[Any, Any]]]:
     """Compare actions at two parsers"""
     actions_1 = parser_1._actions  # pylint: disable=protected-access
     actions_2 = parser_2._actions  # pylint: disable=protected-access
-    if len(actions_1) != len(actions_2):
-        return False
-    # check order of elements
+    res: List[Dict[str, Tuple[Any, Any]]] = []
     for act_1, act_2 in zip(actions_1, actions_2):
-        if not all(
-            val == act_2.__dict__.get(key)
-            for key, val in act_1.__dict__.items()
-            if key != "container"
-        ):
-            return False
-    return True
+        if not actions_same(act_1, act_2):
+            res.append({"action_type": (type(act_1), type(act_2))})
+        else:
+            if not actions_equal(act_1, act_2):
+                res.append(actions_diff(act_1, act_2))
+    return res
 
 
 def parsers_equal(
