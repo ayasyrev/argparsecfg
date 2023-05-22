0dc4962ee04e5f6aac1b8a8d4363027703a96c9c
ayasyrev <a.yasyrev@gmail.com>
2023-05-22 16:51:30 +0300

    alternative flag

--- tests/test_add_argument_field_errors.py
+++ tests/test_add_argument_field_errors.py
@@ -57,6 +57,23 @@ def test_flag_3_wrong_dc_name(capsys: CaptureFixture[str]):
     assert "got `flag` --arg_1 but dc name is arg_2" in out
 
 
+def test_flag_4_long_flag(capsys: CaptureFixture[str]):
+    """test add flag different from dc name"""
+
+    @dataclass
+    class ArgFlag:
+        arg_1: int = field_argument("-a", flag="--a1")
+
+    parser = create_parser()
+    add_args_from_dc(parser, ArgFlag)
+    assert "-a" in parser._option_string_actions
+    assert "--arg_1" not in parser._option_string_actions
+    assert "--a1" in parser._option_string_actions
+    captured = capsys.readouterr()
+    out = captured.out
+    assert out == ""
+
+
 def test_wrong_dest(capsys: CaptureFixture[str]):
     """test add flag different from dc name"""
 
