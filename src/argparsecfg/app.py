c8cdedac28dcb33137bc314db26f8b8414c7ed8e
ayasyrev <a.yasyrev@gmail.com>
2023-05-25 18:01:43 +0300

    wraps app

--- src/argparsecfg/app.py
+++ src/argparsecfg/app.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 from dataclasses import is_dataclass
+from functools import wraps
 from inspect import signature
 from typing import Any, Callable, Optional, Sequence, Type
 
-from argparsecfg.core import ArgumentParserCfg, add_args_from_dc, create_dc_obj, create_parser
+from argparsecfg.core import (ArgumentParserCfg, add_args_from_dc,
+                              create_dc_obj, create_parser)
 
 
 def app(parser_cfg: ArgumentParserCfg | None = None,):
@@ -15,11 +17,11 @@ def app(parser_cfg: ArgumentParserCfg | None = None,):
     # to add - args for argparse parser, ...
 
     def create_app(func: Callable[[Type[Any]], None]):
-
         sig = signature(func)
         params = [param.annotation for param in sig.parameters.values() if is_dataclass(param.annotation)]
         app_cfg = params[0]
 
+        @wraps(func)
         def parse_and_run(args: Optional[Sequence[str]] = None) -> None:
             parser = create_parser(parser_cfg)
             add_args_from_dc(parser, app_cfg)
