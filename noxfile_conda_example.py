472014163313e31f099ee5a0fa42e57930501ca3
ayasyrev <a.yasyrev@gmail.com>
2024-07-09 08:26:35 +0000

    uv + ruff

--- noxfile_conda_example.py
+++ noxfile_conda_example.py
@@ -8,7 +8,7 @@ print(f"examples files: {', '.join(file.name for file in example_files)}")
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"], venv_backend="mamba")
 def tests_examples(session: nox.Session) -> None:
-    session.install("pytest", "pytest-cov")
-    session.install("-e", ".")
+    session.conda_install("uv")
+    session.install("uv", "pip", "install", ".[tests]")
     for filename in example_files:
         session.run("python", str(filename))
--- noxfile_cov.py
+++ noxfile_cov.py
@@ -1,16 +1,18 @@
 import nox
 
 
-@nox.session(python=["3.1"])
+@nox.session(python=["3.11"])
 def cov_tests(session: nox.Session) -> None:
     args = session.posargs or ["--cov"]
-    session.install(".", "pytest", "pytest-cov", "coverage[toml]")
+    session.install("uv")
+    session.run("uv", "pip", "install", ".[tests]")
     session.run("pytest", *args)
 
 
 @nox.session(python="3.11")
 def coverage(session: nox.Session) -> None:
     """Upload coverage data."""
-    session.install("coverage[toml]", "codecov")
+    session.install("uv")
+    session.run("uv", "pip", "install", "coverage[toml]", "codecov")
     session.run("coverage", "xml", "--fail-under=0")
     session.run("codecov", *session.posargs)
