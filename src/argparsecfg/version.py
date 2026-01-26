ce2e87e037d5fb90b8cccb65d6169dbab17fb2ba
ayasyrev <a.yasyrev@gmail.com>
2026-01-26 18:04:08 +0300

    chore: read version from importlib metadata

--- src/argparsecfg/version.py
+++ src/argparsecfg/version.py
@@ -1,1 +1,10 @@
-__version__ = "0.2.5_dev"
+from __future__ import annotations
+
+from importlib import metadata
+from importlib.metadata import PackageNotFoundError
+
+
+try:
+    __version__ = metadata.version("argparsecfg")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
